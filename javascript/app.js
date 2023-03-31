bce6686ea22c1bf73ce7a9a209b9edf5d018040e
MuruaSL <leonardo-murua@hotmail.com.ar>
2023-03-31 08:37:17 -0300

    first commit

---
+++ javascript/app.js
@@ -0,0 +1,13 @@
+let precio = parseInt(prompt("ingrese un precio de hamburguesa para saber como continuar con tu vida"))
+let queso = prompt("Lleva queso extra?? (si o no)")
+
+if (queso == "si") {  
+    precio += 50;
+}
+if (precio<3000){
+        alert("Podrias comprar mas de una burger")
+}else if ((precio>3000) && (precio<5000)){
+        alert("Comprá solo una o te quedas cortina para fin de mes")
+}else{
+        alert("Tu billetera tiene un mensaje: 'Saca la mano de ahí carajo'")
+}
